32c289bdf523c06ac6603972892cb090fa073881
baianacarai <138497780+baianacarai@users.noreply.github.com>
2025-07-08 09:07:42 -0300

    aula 5

---
+++ main.js
@@ -0,0 +1,31 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro.senha__botao');
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+campoSenha.value = letrasMaiusculas;
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho; 
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1){
+    //tamanhoSenha = tamanhoSenha - 1;
+    tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+    //tamanhoSenha = tamanhoSenha + 1;
+    tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+console.log(botoes);
